5f1574e2926ee1b2363e0732b87663bc781864d2
Ahmed Kotb <akotb@talabat.com>
2021-01-23 19:03:03 +0400

    Initial Commit

---
+++ ListingIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ListingIOS
+//
+//  Created by Ahmed Kotb on 23/01/2021.
+//  Copyright © 2021 Ahmed Kotb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
